337f75ae088452236120445c6ef134e95f71b00b
Ian <ian.mwangi1@student.moringaschool.com>
2024-06-13 10:43:46 +0300

    Clean up ignored files

--- lib/__init__.py
+++
@@ -1,5 +0,0 @@
-from event import Event
-from attendee import Attendee
-from tickets import Ticket
-# from .event import event
-
--- lib/config.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-# Establish connection to the database
-conn = sqlite3.connect("db/events.db")
-
-cursor = conn.cursor()
